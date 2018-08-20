82ef80088418aaa84d8c017e69434ba3103f1566
Lightwing Ng <rodney_ng@icloud.com>
2018-08-20 23:50:43 +0800

    insert space

--- src/tmall/util/insertSpace.js
+++ src/tmall/util/insertSpace.js
@@ -1,5 +1,5 @@
-function insert_spacing(str) {
+function insert_spacing(String) {
     const p1 = /([A-Za-z_])([\u4e00-\u9fa5]+)/gi;
     const p2 = /([\u4e00-\u9fa5]+)([A-Za-z_])/gi;
-    return str.replace(p1, "$1 $2").replace(p2, "$1 $2")
+    return String.replace(p1, '$1 $2').replace(p2, '$1 $2');
 }
